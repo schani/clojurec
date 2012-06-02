0bff7b2c8451903313397eaa240be84aeecb06e6
Mark Probst <mark.probst@gmail.com>
2012-06-02 16:42:31 +0200

    Remove clojurescript dependence from project.

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :source-paths ["src/clj"]
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/clojurescript "0.0-1236"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-swank "1.4.4"]
             [lein-pprint "1.1.1"]])
