6ebba185d035a1535bd20f545061eda4b4f6da8e
Mark Probst <mark.probst@gmail.com>
2012-06-06 23:25:01 +0200

    Nice functions for compiling, inspecting and running code.

--- src/clj/clojurec/core.clj
+++ src/clj/clojurec/core.clj
@@ -1,28 +1,52 @@
 (ns clojurec.core
   (:require [clojure.inspector :as inspector]
             [clojure.java.io :as io]
+	    [clojure.java.shell :as shell]
             [cljc.compiler :as cljc]))
 
-(binding [*print-meta* true]
+(defn analyze [expr]
   (cljc/reset-namespaces!)
-  (let [env {:ns (@cljc/namespaces 'cljc.user) :context :statement :locals {}}
-        ast (cljc/analyze env
-			  '(def heusler (fn* ([x] x)))
+  (let [env {:ns (@cljc/namespaces 'cljc.user) :context :statement :locals {}}]
+    (cljc/analyze env expr)))
 
-			  )]
-    (inspector/inspect-tree ast)
-    (let [main-code (with-out-str (cljc/emit ast))
-          code (apply str
-                   (concat @cljc/declarations
-                           ["int main (void) {\n"
-                            "environment_t *env = NULL;\n"
-                            "cljc_init ();\n"
-                            main-code
-                            "return 0;\n}\n"]))
-          user-dir (java.lang.System/getProperty "user.dir")
-          preamble (slurp (io/file user-dir "src" "c" "preamble.c"))]
-      (spit (io/file user-dir "run" "cljc.c") (str preamble code))
-      code)))
+(defn inspect-ast [ast]
+  (inspector/inspect-tree ast))
+
+(defn compile-ast [ast]
+  (let [main-code (with-out-str (cljc/emit ast))]
+    (apply str
+	   (concat @cljc/declarations
+		   ["int main (void) {\n"
+		    "environment_t *env = NULL;\n"
+		    "cljc_init ();\n"
+		    main-code
+		    "return 0;\n}\n"]))))
+
+(defn compile-expr [expr]
+  (compile-ast (analyze expr)))
+
+(defn run-code [code]
+  (let [user-dir (java.lang.System/getProperty "user.dir")
+	run-dir (io/file user-dir "run")
+	preamble (slurp (io/file user-dir "src" "c" "preamble.c"))]
+    (spit (io/file run-dir "cljc.c") (str preamble code))
+    (let [{:keys [exit out]} (shell/sh "make" :dir run-dir)]
+      (if (= exit 0)
+	(let [{:keys [exit out]} (shell/sh "./cljc" :dir run-dir)]
+	  (if (= exit 0)
+	    (read-string (str \[ out \]))
+	    :run-error))
+	:compile-error))))
+
+(defn run-expr [expr]
+  (run-code (compile-expr expr)))
+
+(defn inspect-and-run-expr [expr]
+  (let [ast (analyze expr)]
+    (inspect-ast ast)
+    (run-code (compile-ast ast))))
+
+;;(inspect-and-run-expr '(print 1))
 
 
 (comment
