aac1ab9e128c867e179366691bb347f02b3fb4f9
Mark Probst <mark.probst@gmail.com>
2012-06-17 23:10:28 +0200

    Move core.cljc to a proper classpath.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clojurec "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :source-paths ["src/clj"]
   :dependencies [[org.clojure/clojure "1.4.0"]]
+  :source-paths ["src/clj" "src/cljc"]
   :plugins [[lein-swank "1.4.4"]
             [lein-pprint "1.1.1"]])
