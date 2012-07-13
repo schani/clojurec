dc236bf8551a2b1826e8004380f9f4ba6b436612
Mark Probst <mark.probst@gmail.com>
2012-07-13 17:49:33 +0200

    Remove leiningen plugin dependencies.
    Those plugins should be in the user's profiles.clj.

--- project.clj
+++ project.clj
@@ -2,6 +2,4 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :source-paths ["src/clj" "src/cljc"]
-  :plugins [[lein-swank "1.4.4"]
-            [lein-pprint "1.1.1"]])
+  :source-paths ["src/clj" "src/cljc"])
